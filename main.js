2c22bfd75c924d5b17e4228e31ebbfa1838098c2
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-11 14:18:43 -0500

    [added] more fiels

---
+++ main.js
@@ -0,0 +1,33 @@
+const divChatPopup = document.createElement("div");
+divChatPopup.classList.add("chat-container");
+document.body.appendChild(divChatPopup);
+
+let timer = 5000;
+
+function chatPopup(pic, text, timer) {
+  divChatPopup.innerHTML = `
+    <div class="chat-icon">
+              <img
+                src="${pic}"
+                alt="Profile picture"
+                class="profile-picture"
+              />
+              <div class="online-indicator"></div>
+            </div>
+            <div class="chat-bubble">
+              <!--<button class="close-button">&times;</button>-->
+              <p id="messagebubble" style="display: none; color: #555 !important;">${text}</p>
+              <div class="loading" id="loading">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
+              </div>
+            </div>
+    `;
+    
+
+setTimeout(function () {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("messagebubble").style.display = "block";
+  }, timer);
+}
