b3d647d33dc4c9c7cd765636554a4d7bc6f40cc1
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-11 18:09:07 -0500

    [added] global to use

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-const divChatPopup = document.createElement("div");
-divChatPopup.classList.add("chat-container");
+const aChatPopup = document.createElement("a");
 
-document.body.appendChild(divChatPopup);
+document.body.appendChild(aChatPopup);
 
 let timer = 5000;
 
-function chatPopup({ pic, text, timer }) {
-  divChatPopup.innerHTML = `
+function chatPopup({ pic, text, link, timer }) {
+  aChatPopup.innerHTML = `
   
-    <div class="chat-icon">
-              <img
-                src="${pic}"
-                alt="Profile picture"
-                class="profile-picture"
-              />
-              <div class="online-indicator"></div>
-            </div>
-            <div class="chat-bubble">
-              <!--<button class="close-button">&times;</button>-->
-              <p id="messagebubble" style="display: none; color: #555 !important;">${text}</p>
-              <div class="loading" id="loading">
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-              </div>
-            </div>
-            </a>
+  <a
+      href="${link}"
+      style="text-decoration: none; color: black"
+    >
+      <div class="chat-container">
+        <div class="chat-icon">
+          <img
+            src="${pic}"
+            alt="Profile picture"
+            class="profile-picture"
+          />
+          <div class="online-indicator"></div>
+        </div>
+        
+        <div class="chat-bubble">
+        <div class="triangle"></div>
+          <!--<button class="close-button">&times;</button>-->
+          <p id="messagebubble" style="display: none; color: #555 !important">
+          ${text}
+          </p>
+          <div class="loading" id="loading">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+          </div>
+        </div>
+      </div>
+    </a>
     `;
 
   setTimeout(function () {
